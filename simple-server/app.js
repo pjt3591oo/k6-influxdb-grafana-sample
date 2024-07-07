42c5de398b0e06df4e6eab99e6b04c1ad3f66a20
pjt3591oo <pjt3591oo@gmail.com>
2024-07-07 19:18:27 +0900

    simple-server 수정

--- simple-server/app.js
+++ simple-server/app.js
@@ -1,41 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// app.js
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const http = require('http');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  return res.status(200).json({data: 'Hello World!', method: 'GET'});
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.post('/', (req, res) => {
+  return res.status(201).json({data: 'Hello World!', method: 'POST'});
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  return res.status(err.status || 500).render('error');
 });
 
-module.exports = app;
+const server = http.createServer(app);
+server.listen(8080);
